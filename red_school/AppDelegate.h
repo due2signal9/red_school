40183c174241e696b7c45e4f620e67c2c91d18d4
chenhao <846894154@qq.com>
2018-09-29 15:42:35 +0800

    Initial Commit

---
+++ red_school/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  red_school
+//
+//  Created by Meeno on 2018/9/29.
+//  Copyright © 2018年 red. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ red_school/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  red_school
+//
+//  Created by Meeno on 2018/9/29.
+//  Copyright © 2018年 red. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
